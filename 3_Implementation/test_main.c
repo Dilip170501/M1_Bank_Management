08649db3074071aa28acbe58ac69742dfd2e7f2b
Dilip170501 <101535266+Dilip170501@users.noreply.github.com>
2022-04-03 16:23:24 +0530

    commit

--- 3_Implementation/test_main.c
+++ 3_Implementation/test_main.c
@@ -1,1 +1,35 @@
+#include "unity.h"
+
+#define PROJECT_NAME  "BANK_MANAGEMENT"
+
+/* Prototypes for all the test functions */
+
+/**
+ * @brief Testing function for Bank_management
+ * 
+ */
+extern void test_main();
+
+/* Required by the unity test framework */
+void setUp(void)
+{
+}
+
+/* Required by the unity test framework */
+void tearDown(void)
+{
+}
+
+/* Start of the application test */
+int main(void)
+{
+    /* Initiate the Unity Test Framework */
+    UnityBegin(NULL);
+
+    /* Run Test functions */
+    RUN_TEST(test_main);
+    
+    /* Close the Unity Test Framework */
+    return (UnityEnd());
+}
 
