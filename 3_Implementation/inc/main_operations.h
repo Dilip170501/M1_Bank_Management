9b8ef10e9ca7ef41223aacf108cf02633a43bbc1
Dilip170501 <101535266+Dilip170501@users.noreply.github.com>
2022-04-03 16:51:25 +0530

    commit

--- 3_Implementation/inc/main_operations.h
+++ 3_Implementation/inc/main_operations.h
@@ -1,1 +1,19 @@
+/**
+ * @file main_operations.h
+ * @author Dilip NV (dilipnatarajan1705@gmail.com)
+ * @brief Header file for Bank Management
+ * @version 0.1
+ * @date 2022-03-26
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
+#ifndef _BANK_MANAGEMENT_OPERATIONS_H_
+#define _BANK_MANAGEMENT_OPERATIONS_H_
+
+/**
+ * @brief Shows the record of bank management
+ * @return Text file generated
+ */
+void main():
+#endif /* #define _BANK_MANAGEMENT_H_ */]
