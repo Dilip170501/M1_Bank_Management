0947ec3c70ba84f1c5a796df53473f7069f65844
Dilip170501 <101535266+Dilip170501@users.noreply.github.com>
2022-04-03 17:09:15 +0530

    commit

--- 3_Implementation/src/main_operations.c
+++ 3_Implementation/src/main_operations.c
@@ -1,1 +1,21 @@
+/**
+ * @file main_operations.c
+ * @author DILIP NV (dilipnatarajan1705@gmail.com)
+ * @brief This is the source code which contains all the functions for the project
+ * @version 0.1
+ * @date 2022-03-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <main_operations.h>
+
+/**
+ * @brief Shows the record of bank management
+ * @return Text file generated 
+ */
+void main()
+{
+    return ("main.txt","r");
+}
 
